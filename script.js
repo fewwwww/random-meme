5335b49b87327b580b03c6df0a757f4093e2c0de
Suning Yao <sy2940@nyu.edu>
2022-11-03 01:09:03 -0400

    finish

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const imgCount = 5;
-  const imgId = Math.floor(Math.random() * imgCount);
-  const imgUrl = `imgs/${imgId}.*`;
+const imgCount = 5;
+const imgId = Math.floor(Math.random() * imgCount);
+const imgUrl = `imgs/${imgId}.jpg`;
 
-  const imgTag = document.createElement('img');
-  imgTag.src = imgUrl;
+const imgTag = document.createElement('img');
+imgTag.src = imgUrl;
 
-  const bodyTag = document.getElementsByTagName('body')[0];
-  bodyTag.appendChild(imgTag);
-  bodyTag.style.backgroundColor = getAverageRGB(imgTag);
-});
+const bodyTag = document.getElementsByTagName('body')[0];
+bodyTag.appendChild(imgTag);
+bodyTag.style.backgroundColor = getAverageRGB(imgTag);
