c2e55c1c53626d8f57a8f4efaa33e4810848665c
Suning Yao <sy2940@nyu.edu>
2022-11-03 12:28:15 -0400

    unexpected edge case

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 // constants based on start.js's log
 const imgCount = 1094;
-const gifCount = 619;
-const imgPercentage = 63;
-const gifPercentage = 37;
+// const gifCount = 619;
+// const imgPercentage = 63;
+// const gifPercentage = 37;
 
 // get random img path
-let imgUrl;
-if (Math.floor(Math.random() * 101) <= imgPercentage) {
-  const imgId = Math.floor(Math.random() * imgCount);
-  imgUrl = `imgs/${imgId}.JPG`;
-} else {
-  const gifId = Math.floor(Math.random() * gifCount);
-  imgUrl = `imgs/${gifId}.GIF`;
+// let imgUrl;
+// if (Math.floor(Math.random() * 101) <= imgPercentage) {
+const imgId = Math.floor(Math.random() * imgCount);
+imgUrl = `imgs/${imgId}.JPG`;
+// unexpected error happens when using JPG
+if (imgId === 1 || imgId === 0) {
+  imgUrl = `imgs/${imgId}.jpg`;
 }
+// } else {
+// const gifId = Math.floor(Math.random() * gifCount);
+// imgUrl = `imgs/${gifId}.GIF`;
+// }
 
 // make img tag
 const imgTag = document.createElement('img');
