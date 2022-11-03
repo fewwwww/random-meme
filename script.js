cead65279ad4f6e919905ac6509c394f1a1b7033
Suning Yao <sy2940@nyu.edu>
2022-11-03 01:04:54 -0400

    finish

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const imgCount = 5;
+  const imgId = Math.floor(Math.random() * imgCount);
+  const imgUrl = `imgs/${imgId}.*`;
+
+  const imgTag = document.createElement('img');
+  imgTag.src = imgUrl;
+
+  const bodyTag = document.getElementsByTagName('body')[0];
+  bodyTag.appendChild(imgTag);
+  bodyTag.style.backgroundColor = getAverageRGB(imgTag);
+});
