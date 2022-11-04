8cef55b01500fbc4ee9d085afa9fb51eddcadd5c
Edalbegam <111589348+Edalbegam@users.noreply.github.com>
2022-11-04 17:11:42 +0800

    Update script.js
    Pad the random number to match padded image file name.

--- script.js
+++ script.js
@@ -3,11 +3,20 @@ const imgCount = 1094;
 // const gifCount = 619;
 // const imgPercentage = 63;
 // const gifPercentage = 37;
+const MAX_FILE_NUMBER_LENGTH=8
+
+function pad(num,len) {
+    numstr=num.toString();
+    while (numstr.length<len){
+      numstr='0'+numstr;
+    }
+    return numstr;
+  }
 
 // get random img path
 // let imgUrl;
 // if (Math.floor(Math.random() * 101) <= imgPercentage) {
-const imgId = Math.floor(Math.random() * imgCount);
+const imgId = pad(Math.floor(Math.random() * imgCount),MAX_FILE_NUMBER_LENGTH);
 imgUrl = `imgs/${imgId}.JPG`;
 // unexpected error happens when using JPG
 if (imgId === 1 || imgId === 0) {
